1252efdc2dc3a7ee7e1b6caa5bd94f8ed1c1e123
sofiasls <sofiasls@gmail.com>
2026-07-02 20:51:45 -0300

    alteração de pasta

--- scripts/obterdatas.js
+++
@@ -1,5 +0,0 @@
-const anoAtual = new Date().getFullYear();
-document.getElementById('anoatual').textContent = anoAtual;
-
-document.getElementById('ultimaModificacao').textContent =
-  'Última modificação: ' + document.lastModified;
